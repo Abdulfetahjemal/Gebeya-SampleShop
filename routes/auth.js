9505631186cc15f7a354fd1a00fb4daee08e4216
Abdulfetahjemal <32792371+Abdulfetahjemal@users.noreply.github.com>
2020-11-10 10:06:24 +0300

    login done

---
+++ routes/auth.js
@@ -0,0 +1,17 @@
+var jwt = require('jsonwebtoken');
+
+async function isAuthorized(req, res, next) {
+    idToken = req.header('authorization');
+    if (idToken !== undefined) {
+        jwt.verify(idToken, 'ForDemoOnlllny', function (err, decoded) {
+            if (decoded == undefined) {
+                res.status(200).json({ message: "Wrong token" })
+            } else {
+                next()
+            }
+        });
+    } else res.status(400).json({ message: 'Auth token required' });
+};
+
+
+module.exports = isAuthorized
--- routes/product.js
+++ routes/product.js
@@ -1,9 +1,9 @@
 var express = require('express')
 var router = express.Router()
-
+const isauthorized = require('./auth');
 
 // define the home page route
-router.get('/', function (req, res) {
+router.get('/', isauthorized, function (req, res) {
     res.status(200).json({ message: "Product added Succsusfuly " })
 })
 
