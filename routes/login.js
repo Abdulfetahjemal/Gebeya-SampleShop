ffacf5201909e5ec6eeb54a08426b5f633978d00
Abdulfetahjemal <32792371+Abdulfetahjemal@users.noreply.github.com>
2020-11-10 09:58:46 +0300

    logindone

---
+++ routes/login.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser')
+const bcrypt = require('bcrypt');
+const con = require('./dbservice')[1];
+const Joi = require('joi');
+var jwt = require('jsonwebtoken');
+
+router.use(bodyParser.json())
+router.post('/', (req, res) => {
+
+    let formdata = RegstrationUserSchema.validate({
+        email: req.body["email"],
+        password: req.body["password"],
+
+    });
+    if (formdata.error) {
+        res.status(400).json({ message: formdata.error.details[0]["message"] })
+    }
+    else {
+
+        con.query("Select password from Users where email = '" + req.body["email"] + "'", function (err, result) {
+            bcrypt.compare(req.body["password"], result[0].password, function (err, result) {
+                if (result)
+                    res.send(jwt.sign({ email: req.body["email"] }, 'ForDemoOnlllny'))
+                else
+                    res.send('Login failed')
+            });
+        })
+
+    }
+})
+
+
+const RegstrationUserSchema = Joi.object({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(6).max(255),
+
+})
+module.exports = router;
--- routes/main.js
+++ routes/main.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const userRegstration = require('./regstration');
 const product = require('./product')
+const userLogin = require('./login')
 
-
+router.use('/user/login', userLogin)
 router.use('/user/regstration', userRegstration)
 router.use('/product', product)
 
